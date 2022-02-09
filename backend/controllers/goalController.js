c4bbbd3eb3d47ff19f40e5df68bf523aa0fb1c85
Harold Fich <haroldfich@gmail.com>
2022-02-09 13:58:11 -0500

    basic CRUD routes and controller

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc   GET goals
+// @route  GET /api/goals
+// @access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "GET goals" });
+};
+
+// @desc   SET goal
+// @route  POST /api/goals
+// @access Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "SET goals" });
+};
+
+// @desc   UPDATE goal
+// @route  PUT /api/goals/:id
+// @access Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `UPDATE goals ${req.params.id}` });
+};
+
+// @desc   DELETE goal
+// @route  GET /api/goals/:id
+// @access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `DELETE goals ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
